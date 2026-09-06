25da1fc72c2e6ef259a23c626ad2b8063cd48002
yusufihsane <ihsanyusuf230@gmail.com>
2026-09-06 15:38:28 +0300

    Change model version from 3.7 to 3.6

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def sor():
 
                 response = client.models.generate_content(
 
-                    model="gemini-3.7-flash-lite",
+                    model="gemini-3.6-flash",
 
                     contents=[
                         soru,
@@ -188,7 +188,7 @@ def sor():
 
             response = client.models.generate_content(
 
-                model="gemini-3.7-flash-lite",
+                model="gemini-3.6-flash",
 
                 contents=soru
 
